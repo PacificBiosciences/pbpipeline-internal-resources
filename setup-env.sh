b45933db9438b7d05f8500d0f998b8dbc20772a1
mpkocher <michael.kocher@me.com>
2016-07-14 12:38:38 -0700

    Update setup-env.sh to load view resources

--- setup-env.sh
+++ setup-env.sh
@@ -1,3 +1,7 @@
 export PROJ_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export PB_PIPELINE_TEMPLATE_DIR=${PROJ_DIR}/resolved-pipeline-templates
 export PB_TOOL_CONTRACT_DIR=${PROJ_DIR}/tool-contracts
+
+# View Rules
+export PB_RULES_REPORT_VIEW_DIR=${PROJ_DIR}/rules-report-view
+export PB_RULES_PIPELINE_VIEW_DIR=${PROJ_DIR}/rules-pipeline-template-view
