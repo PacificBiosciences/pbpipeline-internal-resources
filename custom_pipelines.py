afde6303a3b2db8d1fdf1f82570a67f59b16ca3b
Martin D. Smith <mdsmith@pacificbiosciences.com>
2016-07-28 10:25:20 -0700

    Resolved eol-qc pipeline first commit

--- custom_pipelines.py
+++ custom_pipelines.py
@@ -58,10 +58,11 @@ def to_bs():
 def to_bs():
     """EOL QC custom resequencing pipeline"""
 
-    b3 = [(Constants.ENTRY_DS_SUBREAD, 'pbinternal2.tasks.eol_qc_stats:0'),
-          ("pbsmrtpipe.pipelines.sa3_ds_resequencing_fat:pbalign.tasks.pbalign:0",
-           'pbinternal2.tasks.eol_qc_stats:1')]
-    return b3
+    b1 = [(Constants.ENTRY_DS_SUBREAD, 'pbsmrtpipe_internal.tasks.eol_qc:0'),
+          (Constants.ENTRY_DS_ALIGN, 'pbsmrtpipe_internal.tasks.eol_qc:1')]
+          #("pbsmrtpipe.pipelines.sa3_ds_resequencing_fat:pbalign.tasks.pbalign:0",
+          # 'pbinternal2.tasks.eol_qc_stats:1')]
+    return b1
 
 
 @registry("internal_cond_dev", "Dev Reseq Cond Report", "0.2.0", tags=C.TAGS_DEFAULT)
